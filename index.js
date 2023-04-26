864f3865a2fd105cd775a5b36aac6f90abcd3d5b
Aluno talentos <raul.hiago.salomao@gmail.com>
2023-04-25 21:49:32 -0300

    Exercicios-Operadores

---
+++ index.js
@@ -0,0 +1,67 @@
+
+//Exercice 1)
+
+//1)
+
+/*let firstValue = 3
+let secondValue = 4
+
+
+const result = firstValue + secondValue
+
+console.log (result) */ 
+
+//2)
+
+/*let firstValue = 3
+let secondValue = 5
+const result = firstValue * secondValue / 2
+
+console.log (result) */
+
+//3)
+
+/*let firstValue = 4
+let secondValue = 5
+
+const result = (firstValue - secondValue) * -1
+console.log (result)*/
+
+//4)
+
+/*let firstValue = 234
+const result = firstValue % 5
+
+console.log (result)*/
+
+/*const firstNumber = 10
+const secondNumber = 20
+
+console.log (firstNumber===secondNumber)
+console.log (firstNumber!==secondNumber)
+console.log (firstNumber>secondNumber)
+console.log (firstNumber>=secondNumber)
+console.log (firstNumber<secondNumber)
+console.log (firstNumber<=secondNumber)*/
+
+//Exercice 5)
+
+// let theName = prompt("what is your name?")
+// let age = Number(prompt ("what is your age?"))
+// const legalAge = (age>=18)
+// const ageIn2050 = (age + 27)
+
+// console.log ("name =", theName)
+// console.log ("age =", age)
+// console.log ("are you under 18? =", legalAge)
+// console.log ("Your age in 2050 =", ageIn2050)
+
+
+
+
+
+
+
+
+
+
